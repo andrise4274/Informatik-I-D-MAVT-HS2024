6ee7a3d6edbb40913ec4f2747a5cce69a490f741
andrise4274 <andri.seiz@breitband.ch>
2024-11-26 09:29:48 +0100

    solution to PSet11 dynamic queue added

--- Exercise 11_ Dynamic Memory, Pointers, Dynamic Data Structures I/Task 3_ Dynamic Queue/queue.cpp
+++ Exercise 11_ Dynamic Memory, Pointers, Dynamic Data Structures I/Task 3_ Dynamic Queue/queue.cpp
@@ -3,20 +3,24 @@
 #include "queue.h"
 
 /////////////////////////////////////////////////////////////////////////////
-// IMPLEMENTATION
+// recursive helper function
+void print_element_after(Node *n);
 /////////////////////////////////////////////////////////////////////////////
 
-Queue::Queue() : first(nullptr), last(nullptr) {
-  
-}
+Queue::Queue() : first(nullptr), last(nullptr) {}
+
 
 void Queue::enqueue(int value) {
-  
-  if (first == nullptr) { // Case 1: empty list
-    first = new Node(value, nullptr);
-  } else { // Case 2: non-empty list (code as before)
-    Node* temp = new Node(value, first);
-    first = temp;
+  Node* new_node = new Node(value, nullptr);
+  // if the list is empty, set first AND last to pointer to new_node
+  if (first == nullptr) {
+    first = new_node;
+    last = new_node;
+  // if list isn't empty - let current last to point to new_node, 
+  //set last pointer to new_node
+  } else { 
+    last->next = new_node;
+    last = new_node;
   }
 
 }
@@ -26,21 +30,39 @@ int Queue::dequeue() {
   if (is_empty()) {
     return 1;
   }
+  Node *node_to_remove = first;
+  // set the beginning of linked list to 2nd element
   first = first->next;
-  
-  return 0;
+  // if list is empty now, ensure last is nullptr too
+  if (first == nullptr) {
+    last = nullptr;
+  }
+  // get the return value
+  int val = node_to_remove->value;
+  // free allocated memory of deleted node (similar to malloc() and free() in C)
+  // memory was allocated in enqueue function with new keyword
+  delete node_to_remove;
+  return val;
 }
 
+
 bool Queue::is_empty() const {
-  if (first != nullptr && last != nullptr) {
+  if (first == nullptr) {
     return true;
   }
   return false;
 }
 
+
 void Queue::print_reverse() const {
-  // TODO
+  std::cout << "[";
+  // recursive call
+  if (first != nullptr) {
+    print_element_after(first);
+  }
+  std::cout << "]" << std::endl;
 }
+
 
 // PRE: -
 // POST: print the sequence of integers in nodes reachable from n,
@@ -60,4 +82,18 @@ void Queue::print() const {
     print_node(first->next);
   }
   std::cout << "]";
+}
+
+
+// recursive helper function
+// PRE: takes a pointer to Node
+// POST: prints all values of Nodes which come after Node
+void print_element_after(Node *n) {
+  if (n->next != nullptr) {
+    // recursive call
+    print_element_after(n->next);
+    std::cout << " " << n->value;
+  } else {
+    std::cout << n->value;
+  }
 }
